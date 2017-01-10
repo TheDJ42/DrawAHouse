53c0fcd84823e74e793c129598e1cbc316d2a377
Dylan Johnson <TheDJ42@github.com>
2017-01-09 20:36:55 -0700

    This changes the color to black and names the window "Draw a house"

--- DrawAHouse/Form1.Designer.cs
+++ DrawAHouse/Form1.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(584, 331);
+            this.Name = "Form1";
+            this.Text = "Draw a House";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
